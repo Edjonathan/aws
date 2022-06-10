cf9e94d3bb50a5e14ef141aff8e2a107c26c2d96
gedjona <edjonathan.guedes@gmail.com>
2022-06-10 08:52:55 -0300

    Alteracoes bucket s23

--- terraform/s3/main.tf
+++ terraform/s3/main.tf
@@ -1,27 +1,30 @@
 terraform {
-    required_version = "1.2.2"
-    required_providers {
-      aws = {
-          source = "hashicorp/aws"
-          version = "4.18.0" 
-      }
+  required_version = "1.2.2"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.18.0"
     }
+  }
 }
 
 provider "aws" {
-    region = "us-east-1"
-    #Not Recommended
-    #access_key =
-    ##secret_key = 
-    profile = "terraform" 
+  region = "us-east-1"
+  #Not Recommended
+  #access_key =
+  ##secret_key = 
+  profile = "terraform"
 }
 
-resource "aws_s3_bucket" "my-first-bucket"{
-    
-    bucket = "my-first-bucket-edjonathan"
-    acl    = "private"
-    tags = {
-        Name = "My Bucket"
-        Environment = "Dev"
-    }
+resource "aws_s3_bucket" "my-first-bucket" {
+
+  bucket = "my-first-bucket-edjonathan-1010"
+  acl    = "private"
+  tags = {
+    name        = "My Bucket"
+    environment = "Dev"
+    managedBy   = "Terraform"
+    owner       = "Edjonathan"
+    updatedAt   = "2022-06-10"
+  }
 }
