2c0a3c9511efdf37962e03d55fa8e952d526572a
gedjona <edjonathan.guedes@gmail.com>
2022-08-18 21:09:03 -0300

    Setup inicial ecs

--- terraform/ec2/main.tf
+++ terraform/ec2/main.tf
@@ -16,8 +16,68 @@ provider "aws" {
   profile = var.aws_profile
 }
 
-resource "aws_instance" "web" {
-  ami           = var.instance_ami
-  instance_type = var.instamce_type
-  tags          = var.instance_tags
+data "aws_availability_zones" "this" {
+  state = "available"
 }
+
+resource "aws_subnet" "public" {
+  count                   = 3
+  cidr_block              = cidrsubnet(aws_vpc.this.cidr_block, 8, 2 + count.index)
+  availability_zone       = data.aws_availability_zones.this.names[count.index]
+  vpc_id                  = aws_vpc.default.id
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "private" {
+  count             = 2
+  cidr_block        = cidrsubnet(aws_vpc.default.cidr_block, 8, count.index)
+  availability_zone = data.aws_availability_zones.this.names[count.index]
+  vpc_id            = aws_vpc.default.id
+}
+
+resource "aws_vpc" "this" {
+  cidr_block = "10.0.0.0/16"  
+  tags = {
+    Name = "vpc-ecs-demo"
+  }
+}
+
+resource "aws_security_group" "this" {
+  name        = "example-alb-security-group"
+  vpc_id      = aws_vpc.this.id
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_ecs_cluster" "this" {
+  name = "cluster-demo"
+
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+}
+
+resource "aws_ecs_service" "this" {
+  name            = "hello-world-service"
+  cluster         = aws_ecs_cluster.this.id
+  task_definition = aws_ecs_task_definition.hello_world.arn
+  desired_count   = var.app_count
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    security_groups = [aws_security_group.this.id]
+    subnets         = aws_subnet.private.*.id
+  }
